2b6b7c0c8591e0074c07b303d81fe335966363e0
Germey <cqc@cuiqingcai.com>
2020-07-30 10:26:27 +0800

    to distributed

--- scrapypyppeteer/settings.py
+++ scrapypyppeteer/settings.py
@@ -91,3 +91,8 @@ DOWNLOADER_MIDDLEWARES = {
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+SCHEDULER = "scrapy_redis.scheduler.Scheduler"
+REDIS_URL = 'redis://localhost:6379'
+DUPEFILTER_CLASS = "scrapy_redis.dupefilter.RFPDupeFilter"
+SCHEDULER_PERSIST = True
